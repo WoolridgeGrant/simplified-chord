9043a76f08ba9a476c01d4caa9d3087d9325885a
Woolridgegrant <porteedm@gmail.com>
2017-04-05 11:45:43 +0200

    [Implem1] Refactor responsibility test in is_responsible

--- chord.c
+++ chord.c
@@ -11,7 +11,7 @@
 #define TAGTERM 2 /*Message de terminaison*/
 #define TAGQUIT 3
 #define TAGQUIT_SPREAD 4
-#define DATA_RECHERCHE 9/*Modifier cette variable pour chercher le responsable d'une donnee a partir de son id*/
+#define DATA_RECHERCHE 9/*Modifier cette variable pour chercher le responsable d'une donnee a partir de son id (9, 36, 60 sont de bonnes valeurs)*/
 #define QUIT_RANK 1
 
 struct successor{
@@ -97,6 +97,26 @@ void simulateur(void) {
 	}
 }
 
+/**
+  * is_responsible - Teste si un noeud est responsable d'une donnée ou non
+  *
+  * @resp: id de la première donnée dont le noeud est responsable
+  *
+  * @id_chord: id chord du noeud
+  * 
+  * @id_data_recherche : id de la donnée recherchée
+  *
+  * @return: retourne 1 si la donnée recherchée est gérée par le noeud appelant is_responsible, sinon retourne 0 
+  */
+
+int is_responsible(int resp, int id_chord, int id_data_recherche){
+	printf("is_responsible\n");
+	if(id_chord < resp)
+		return ((id_data_recherche >= resp && id_data_recherche <= NB_DATA) || (id_data_recherche >= 0 && id_data_recherche <= id_chord));
+	else
+		return (id_data_recherche <= id_chord && id_data_recherche >= resp);
+}
+
 void test_initialisation(int rang){
 	//Initialisation
 	int id_chord;
@@ -140,37 +160,21 @@ void test_initialisation(int rang){
 			if(rang_mpi_succ != initiateur)
  				MPI_Ssend(&initiateur, 1, MPI_INT, rang_mpi_succ, TAGTERM, MPI_COMM_WORLD);
 		}
+		else if(is_responsible(resp, id_chord, id_data_recherche)){
+			initiateur = status.MPI_TAG;
+			//Ce noeud est responsable de la donnee recherchee
+			printf("Responsable trouve : %d\n", id_chord);
+			MPI_Ssend(&id_chord, 1, MPI_INT, initiateur, TAGRESP, MPI_COMM_WORLD);	
+			//Envoi du message de terminaison a tout ceux qui suivent
+			if(rang_mpi_succ != initiateur)
+				MPI_Ssend(&initiateur, 1, MPI_INT, rang_mpi_succ, TAGTERM, MPI_COMM_WORLD); //Comme on utilise TAGTERM on envoie l'id de l'initiateur directement
+		}
 		else{
 			initiateur = status.MPI_TAG;
-			if(id_chord < resp){
-				if((id_data_recherche >= resp && id_data_recherche <= NB_DATA) || (id_data_recherche >= 0 && id_data_recherche <= id_chord)){
-					//Ce noeud est responsable de la donnee recherchee
-					printf("Responsable trouve : %d\n", id_chord);
-					MPI_Ssend(&id_chord, 1, MPI_INT, initiateur, TAGRESP, MPI_COMM_WORLD);	
-					//Envoi du message de terminaison a tout ceux qui suivent
-					if(rang_mpi_succ != initiateur)
-						MPI_Ssend(&initiateur, 1, MPI_INT, rang_mpi_succ, TAGTERM, MPI_COMM_WORLD); //Comme on utilise TAGTERM on envoie l'id de l'initiateur directement
-				}
-				else{
-					//Il envoie au successeur	
-					MPI_Ssend(&id_data_recherche, 1, MPI_INT, rang_mpi_succ, initiateur, MPI_COMM_WORLD);	
-				}
-			}
-			else{
-				if(id_data_recherche <= id_chord && id_data_recherche >= resp){
-					//Ce noeud est responsable de la donnee recherchee
-					printf("Responsable trouve : %d\n", id_chord);
-					MPI_Ssend(&id_chord, 1, MPI_INT, initiateur, TAGRESP, MPI_COMM_WORLD);	
-					//Envoi du message de terminaison a tout ceux qui suivent
-					if(rang_mpi_succ != initiateur)
-						MPI_Ssend(&initiateur, 1, MPI_INT, rang_mpi_succ, TAGTERM, MPI_COMM_WORLD); //Comme on utilise TAGTERM on envoie l'id de l'initiateur directement
-				}
-				else{
-					//Il envoie au successeur	
-					MPI_Ssend(&id_data_recherche, 1, MPI_INT, rang_mpi_succ, initiateur, MPI_COMM_WORLD);	
-				}	
-			}
+			//Il envoie au successeur	
+			MPI_Send(&id_data_recherche, 1, MPI_INT, rang_mpi_succ, initiateur, MPI_COMM_WORLD);	
 		}
+	
 	}
 
 	/*
